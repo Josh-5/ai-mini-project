627bb8d66768f0922e363e8dfa0dac02f685225c
Brandon Hoang <brandonh03@vt.edu>
2024-04-11 15:24:19 -0400

    got fds, trying redirect

--- game.py
+++ game.py
@@ -8,19 +8,20 @@
 # Use subprocess to interact: https://docs.python.org/3/library/subprocess.html
 
 import subprocess
+import os
 
 def main():
-    pipe = [0,1]
-    game = subprocess.Popen(["klondike"])
+    inpipe = os.open("input.txt", os.O_RDONLY | os.O_CREAT)
+    outpipe = os.open("output.txt", os.O_WRONLY | os.O_CREAT)
+    print(inpipe)
+    print(outpipe)
+    game = subprocess.Popen(["klondike"], stdin=inpipe, stdout=outpipe)
     """
     https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate
     """
-    communicate("help")
 
-
-   
-
-    
+    os.close(inpipe)
+    os.close(outpipe)
 
 
 if __name__ == "__main__":
