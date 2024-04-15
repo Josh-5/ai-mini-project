57d40e4f61a6b29335ebaab3e77319e4ce9af5bb
Brandon Hoang <brandonh03@hazelnut.rlogin>
2024-04-15 13:36:10 -0400

    Added more helper methods/abstractions to interact with the game

--- game.py
+++ game.py
@@ -10,8 +10,29 @@
 import subprocess
 import time
 
-def move(game, origin:int, card:int, pile:int):
-    game.stdin.write("t " + str(origin) + " " + str(card) + " " + str(pile) + "\n")
+def foundation(game, suit:str, origin:int):
+    game.stdin.write("foundation " + suit + " " + str(origin))
+    delay(game)
+
+def deal(game):
+    game.stdin.write("deal")
+    delay(game)
+
+def waste(game, destination:int):
+    game.stdin.write("waste " + str(destination))
+    delay(game)
+
+def undo(game):
+    game.stdin.write("undo\n")
+    delay(game)
+
+def restart(game):
+    game.stdin.write("new\n")
+    delay(game)
+
+def move(game, origin:int, card:int, destination:int):
+    game.stdin.write("t " + str(origin) + " " + str(card) + " " + str(destination) + "\n")
+    delay(game)
 
 def delay(game):
     game.stdin.flush()
@@ -24,11 +45,19 @@ def main():
     Moves can be written as long as stdin is open. 
 
     """
+    # Startup
     game = subprocess.Popen(["klondike"], stdin=subprocess.PIPE, text=True)
+    restart(game)
+
+
     move(game, 1,0,0)
-    delay(game)
     move(game, 0,0,1)
-    delay(game)
+
+
+
+
+
+    # Shutdown
     game.stdin.close()
     game.wait()
 
