cf85b7501d6e697391ccdd1fa4267b5429b83128
Brandon Hoang <brandonh03@hazelnut.rlogin>
2024-04-15 13:41:01 -0400

    Comments and another method

--- game.py
+++ game.py
@@ -10,30 +10,42 @@
 import subprocess
 import time
 
+# Move all cards to foundations if they're all face up
+def solve(game):
+    game.stdin.write("solve")
+    delay(game)
+
+# Move a card from the origin tableau to the (suit) foundation
 def foundation(game, suit:str, origin:int):
     game.stdin.write("foundation " + suit + " " + str(origin))
     delay(game)
 
+# Draw a card from the stock
 def deal(game):
     game.stdin.write("deal")
     delay(game)
 
+# Move the top card from the stock to a destination tableau
 def waste(game, destination:int):
     game.stdin.write("waste " + str(destination))
     delay(game)
 
+# Undo the most recent move
 def undo(game):
     game.stdin.write("undo\n")
     delay(game)
 
+# Creates a new game
 def restart(game):
     game.stdin.write("new\n")
     delay(game)
 
+# Move the card at the index "card" from the origin tableau to the destination tableau
 def move(game, origin:int, card:int, destination:int):
     game.stdin.write("t " + str(origin) + " " + str(card) + " " + str(destination) + "\n")
     delay(game)
 
+# Internal sleeping to show the moves more clearly
 def delay(game):
     game.stdin.flush()
     time.sleep(1)
