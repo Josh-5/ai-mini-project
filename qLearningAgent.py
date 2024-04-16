d0619bd9abccd89ddd0aa22cef2cfc7592a95869
Brandon Hoang <brandonh03@hornbeam.rlogin>
2024-04-16 14:39:05 -0400

    pivot to another file.

---
+++ qLearningAgent.py
@@ -0,0 +1,13 @@
+#follow to install pytience
+#https://pypi.org/project/pytience/
+
+from pytience.cmd.klondike import KlondikeCmd
+#See game.py for comments about how to use this.
+
+class QLearningAgent():
+
+
+    def __init__(self):
+        self.cmd = KlondikeCmd()
+        self.game = self.cmd.klondike()
+        
