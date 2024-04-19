88ae19835a30637e6f188537040ed2caeb30d5af
Brandon Hoang <brandonh03@sassafras.rlogin>
2024-04-19 15:39:08 -0400

    Merge

--- qLearningAgent.py
+++ qLearningAgent.py
@@ -238,20 +238,18 @@ class QLearningAgent():
         """
         if actionFn == None:
             actionFn = lambda state: state.getLegalActions()
-
         self.actionFn = actionFn #Action fn gives the set of legal moves at the current state
-
-        self.suits = ["♦","♥","♠","♣"]
-
         self.episodesSoFar = 0
         self.accumTrainRewards = 0.0
         self.accumTestRewards = 0.0
-
-        
         self.numTraining = int(numTraining)
         self.epsilon = float(epsilon)
         self.alpha = float(alpha)
         self.discount = float(gamma)
+
+
+
+        self.suits = ["♦","♥","♠","♣"]
 
         self.values = Counter()
 
@@ -380,7 +378,8 @@ class QLearningAgent():
         return self.computeValueFromQValues(state)
 
 
-
+    def run(self):
+        
 
 
 
@@ -388,7 +387,7 @@ class QLearningAgent():
 
 
      
-    #This is where i think we dont nee dthings anymore
+    # Training
 
 
     def observeTransition(self, state,action,nextState,deltaReward):
@@ -446,6 +445,45 @@ class QLearningAgent():
         """
         self.lastState = state
         self.lastAction = action
+
+       def final(self, state):
+        """
+          Called by Pacman game at the terminal state
+        """
+        deltaReward = state.getScore() - self.lastState.getScore()
+        self.observeTransition(self.lastState, self.lastAction, state, deltaReward)
+        self.stopEpisode()
+
+        # Make sure we have this var
+        if not 'episodeStartTime' in self.__dict__:
+            self.episodeStartTime = time.time()
+        if not 'lastWindowAccumRewards' in self.__dict__:
+            self.lastWindowAccumRewards = 0.0
+        self.lastWindowAccumRewards += state.getScore()
+
+        NUM_EPS_UPDATE = 100
+        if self.episodesSoFar % NUM_EPS_UPDATE == 0:
+            print('Reinforcement Learning Status:')
+            windowAvg = self.lastWindowAccumRewards / float(NUM_EPS_UPDATE)
+            if self.episodesSoFar <= self.numTraining:
+                trainAvg = self.accumTrainRewards / float(self.episodesSoFar)
+                print('\tCompleted %d out of %d training episodes' % (
+                       self.episodesSoFar,self.numTraining))
+                print('\tAverage Rewards over all training: %.2f' % (
+                        trainAvg))
+            else:
+                testAvg = float(self.accumTestRewards) / (self.episodesSoFar - self.numTraining)
+                print('\tCompleted %d test episodes' % (self.episodesSoFar - self.numTraining))
+                print('\tAverage Rewards over testing: %.2f' % testAvg)
+            print('\tAverage Rewards for last %d episodes: %.2f'  % (
+                    NUM_EPS_UPDATE,windowAvg))
+            print('\tEpisode took %.2f seconds' % (time.time() - self.episodeStartTime))
+            self.lastWindowAccumRewards = 0.0
+            self.episodeStartTime = time.time()
+
+        if self.episodesSoFar == self.numTraining:
+            msg = 'Training Done (turning off epsilon and alpha)'
+            print('%s\n%s' % (msg,'-' * len(msg)))
 
     # Pacman specifically hasmore functions to track the current state/observation, initial state, and final state.
 
