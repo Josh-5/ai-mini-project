c032661356c15cb3a2bdb1dd6d99978dd211b31e
fattysun1121 <85023400+fattysun1121@users.noreply.github.com>
2024-04-23 11:39:50 -0400

    Tried changing FeatureExtractor, passing it on to Brandon

--- solitaire_ai.py
+++ solitaire_ai.py
@@ -1,51 +1,6 @@
 import random
 
-class FeatureExtractor:
-    def getFeatures(self, state, action):
-        features = dict()
-        tableaus = game.tableau.piles
-        features["bias"] = 1.0
 
-        parse = action.split()
-
-        hiddenCardsPre = 0.0
-        for tableau in tableaus:
-            for card in tableau:
-                if not card.is_revealed:
-                    hiddenCardsPre += 1.0
-
-        util.doAction(game, action)
-
-        hiddenCards = 0.0
-        maxHiddenCards = 0.0
-        for tableau in tableaus:
-            pileHiddenCards = 0.0
-            for card in tableau:
-                if not card.is_revealed:
-                    hiddenCards += 1.0
-                    pileHiddenCards += 1.0
-            if maxHiddenCards < pileHiddenCards:
-                maxHiddenCards = pileHiddenCards
-
-        features["total hidden cards"] = hiddenCards
-        features["max hidden cards"] = maxHiddenCards
-        features["reveal hidden cards"] = hiddenCards < hiddenCardsPre
-
-        if (action[0] == "D"):
-            game.undo_deal()
-        elif (action[0] == "F"):
-            features["origin"] = int(parse[1])
-            features["foundation"] += 1.0
-            game.undo_select_foundation()
-        elif (action[0] == "W"):
-            game.undo_select_waste()
-        elif (action[0] == "T"):
-            features["origin"] = parse[1]
-            game.undo_select_tableau()
-
-
-        features.divideAll(10.0)
-        return features
 
 class SolitaireGame:
     def __init__(self):
@@ -321,3 +276,51 @@ if __name__ == "__main__":
         game.perform_action(action)
     print("Game Over. Here's the final state:")
     print(game.get_current_state())
+    
+class FeatureExtractor:
+    def getFeatures(self, game: SolitaireGame, action):
+        state = game.get_current_state()
+        features = dict()
+        tableaus = state[0]
+        features["bias"] = 1.0
+
+        parse = action.split()
+
+        hiddenCardsPre = 0.0
+        for tableau in tableaus:
+            for card in tableau:
+                if not card.is_revealed:
+                    hiddenCardsPre += 1.0
+
+        util.doAction(game, action)
+
+        hiddenCards = 0.0
+        maxHiddenCards = 0.0
+        for tableau in tableaus:
+            pileHiddenCards = 0.0
+            for card in tableau:
+                if not card.is_revealed:
+                    hiddenCards += 1.0
+                    pileHiddenCards += 1.0
+            if maxHiddenCards < pileHiddenCards:
+                maxHiddenCards = pileHiddenCards
+
+        features["total hidden cards"] = hiddenCards
+        features["max hidden cards"] = maxHiddenCards
+        features["reveal hidden cards"] = hiddenCards < hiddenCardsPre
+
+        if (action[0] == "D"):
+            game.undo_deal()
+        elif (action[0] == "F"):
+            features["origin"] = int(parse[1])
+            features["foundation"] += 1.0
+            game.undo_select_foundation()
+        elif (action[0] == "W"):
+            game.undo_select_waste()
+        elif (action[0] == "T"):
+            features["origin"] = parse[1]
+            game.undo_select_tableau()
+
+
+        features.divideAll(10.0)
+        return features
